845ebb31f642f6cf82378a2daa335dc0444411df
Justin Vendettuoli <jvendettuoli@gmail.com>
2021-02-16 11:41:54 -0800

    ShowTreeMap edited to allow showing multiple trees as well

--- client/src/ShowTreesMap.js
+++ client/src/ShowTreesMap.js
@@ -10,87 +10,30 @@ import {
 	TileLayer
 } from 'react-leaflet';
 
-import {
-	GeoSearchControl,
-	OpenStreetMapProvider
-} from 'leaflet-geosearch';
-import 'leaflet-geosearch/dist/geosearch.css';
-
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+
 import TreePopup from './TreePopup';
 import useStyles from './styles/leafletMap';
 
-// Set up search provider for Leaflet map
-const searchControl = new GeoSearchControl({
-	position   : 'topright',
-	showMarker : false,
-	provider   : new OpenStreetMapProvider()
-});
-
-function ShowTreesMap({
-	setMapCenter,
-	mapCenter,
-	zoomLevel,
-	setGetLocation,
-	getLocation,
-	onGetLocationChange,
-	trees
-}) {
+function ShowTreesMap({ trees }) {
 	const classes = useStyles();
-
-	useEffect(
-		() => {
-			setGetLocation(false);
-		},
-		[ getLocation ]
-	);
-
-	const CenterOnUser = () => {
-		const map = useMap();
-		map.locate({ setView: true });
-		return null;
-	};
-
-	const UpdateCenter = () => {
-		const map = useMapEvents({
-			moveend() {
-				console.log('UpdateCenter - moveend');
-				const coords = map.getCenter();
-				setMapCenter([ coords.lat, coords.lng ]);
-			}
-		});
-		return null;
-	};
-
-	const SearchComponent = () => {
-		const map = useMap();
-		map.addControl(searchControl);
-		// map.on('geosearch/showlocation', () => {
-		// 	console.log('Search Center update');
-		// 	const coords = map.getCenter();
-		// 	setMapCenter([ coords.lat, coords.lng ]);
-		// });
-		return null;
-	};
 
 	const loadingPlaceholder = <CircularProgress />;
 
 	return (
-		<div className="ShowTreesMap">
+		<div className="s">
 			<MapContainer
 				className={classes.mapContainer}
 				placeholder={loadingPlaceholder}
-				center={mapCenter}
-				zoom={zoomLevel}
+				center={[ trees[0].geolocation.y, trees[0].geolocation.x ]}
+				zoom={15}
+				scrollWheelZoom={false}
 			>
 				<TileLayer
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 					attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
 				/>
-				<SearchComponent />
-				{getLocation && <CenterOnUser />}
-				<UpdateCenter />
 				{trees.map((tree) => (
 					<Marker
 						key={`marker-${tree.id}`}
