766dd920e2d3ed8d3e393c698b589d8440a961f6
Justin Vendettuoli <jvendettuoli@gmail.com>
2021-04-16 13:19:53 -0700

    added console.log for trouble shooting deployment

--- server/firebase/firebaseServerAdmin.js
+++ server/firebase/firebaseServerAdmin.js
@@ -2,6 +2,8 @@ const admin = require('firebase-admin');
 const serviceAccount = require('../serviceAccountKeyEnv.js');
 const { DB_URI } = require('../config');
 
+console.log('SERVICE ACCOUNT: ', serviceAccount);
+
 admin.initializeApp({
 	credential  : admin.credential.cert(serviceAccount),
 	databaseURL : DB_URI
