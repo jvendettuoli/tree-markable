c0c96010944dbe586770312d642ef647e432016c
Justin Vendettuoli <jvendettuoli@gmail.com>
2021-04-16 16:50:51 -0700

    Added ssl:{rejectUnauthorized:false} to pg client as per Heroku suggestion

--- server/db.js
+++ server/db.js
@@ -4,7 +4,10 @@ const { Client } = require('pg');
 const { DB_URI } = require('./config');
 
 const db = new Client({
-	connectionString : DB_URI
+	connectionString : DB_URI,
+	ssl              : {
+		rejectUnauthorized : false
+	}
 });
 
 db.connect();
