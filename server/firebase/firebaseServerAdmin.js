6130e4303a17e1aa4874ab293510932a632893ff
Justin Vendettuoli <jvendettuoli@gmail.com>
2021-04-17 16:01:34 -0700

    changed firebase credentials to three required fields for troubleshooting heroku deployment

--- server/firebase/firebaseServerAdmin.js
+++ server/firebase/firebaseServerAdmin.js
@@ -6,8 +6,11 @@ console.log('SERVICE ACCOUNT: ', serviceAccount);
 console.log('DB_URI: ', DB_URI);
 
 admin.initializeApp({
-	credential : admin.credential.cert(serviceAccount)
-	// databaseURL : DB_URI
+	credential : admin.credential.cert({
+		project_id   : process.env.FIREBASE_PROEJCT_ID,
+		private_key  : process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+		client_email : process.env.FIREBASE_CLIENT_EMAIL
+	})
 });
 
 module.exports = admin;
