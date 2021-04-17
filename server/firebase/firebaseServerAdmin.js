23ad2e0bce2b2f5e22ee22f367c20c6be0c31a18
Justin Vendettuoli <jvendettuoli@gmail.com>
2021-04-17 13:06:22 -0700

    removed databaseURL from firebase init

--- server/firebase/firebaseServerAdmin.js
+++ server/firebase/firebaseServerAdmin.js
@@ -3,10 +3,11 @@ const serviceAccount = require('../serviceAccountKeyEnv.js');
 const { DB_URI } = require('../config');
 
 console.log('SERVICE ACCOUNT: ', serviceAccount);
+console.log('DB_URI: ', DB_URI);
 
 admin.initializeApp({
-	credential  : admin.credential.cert(serviceAccount),
-	databaseURL : DB_URI
+	credential : admin.credential.cert(serviceAccount)
+	// databaseURL : DB_URI
 });
 
 module.exports = admin;
